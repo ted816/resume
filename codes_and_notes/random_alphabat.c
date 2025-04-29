86bc27331dfcf9c447ba1d5fd773ab59d44b6762
ted816 <tohjiasen@gmail.com>
2025-04-29 15:56:34 +0800

    Update random_alphabat.c

--- codes_and_notes/random_alphabat.c
+++ codes_and_notes/random_alphabat.c
@@ -1,3 +1,42 @@
+/*
+gcc -o random_alphabat random_alphabat.c
+./random_alphabat
+
+Write a program that generates a “random walk” across a 10 x 10 array. The array will
+contain characters (all ’ . ’ initially). The program must randomly “walk” from element
+to element, always going up. down, left, or right by one element. The elements visited
+by the program will be labeled with the letters A through Z in the order visited. Here’s
+an example of the desired output:
+
+A B . . . . . . . .
+. C D E . . . . . .
+. . . F G H . . . .
+. . . . J I N O . .
+. . . . K L M P . .
+. . . . . . . Q . .
+. . . . U T S R . .
+. . . . V W . . . .
+. . . . . X . . . .
+. . . . . Y Z . . .
+
+I used this example because it showcase the logic flow of basic programmer.
+Everyone would have a different flow of the program and the limitation it has.
+My code have its limitation and cannot detect when there is a dead end.
+Kindly refer to below:
+
+A J I H . . . . . .
+B C F G . . . . . .
+. D E . . . . . . .
+. . . . . . . . . .
+. . . . . . . . . .
+. . . . . . . . . .
+. . . . . . . . . .
+. . . . . . . . . .
+. . . . . . . . . .
+. . . . . . . . . .
+
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
