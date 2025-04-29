11a001f7987346987b77fb4b68e53b841e13ea05
ted816 <tohjiasen@gmail.com>
2025-04-29 15:52:42 +0800

    Update combination.c

--- codes_and_notes/combination.c
+++ codes_and_notes/combination.c
@@ -1,3 +1,19 @@
+/*
+gcc -o combination combination.c -lm
+./combination
+
+This code is used to generate all combination of a 4bit. 
+0000, 0001, 0010, 0011 all the way to 1111.
+
+I came across an question, the user input 4 interger.
+In any combination, can this 4 interger sum up to 100?
+
+This may not be the best way, but using multi dimension array to test all possible combination.
+
+I can statically type the combination into an array.
+But if there is a different number of input each time, this codes is scalable for use.
+*/
+
 #include <stdio.h>
 #include <math.h>
 
