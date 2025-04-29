b47dce469891bf45abc259c2bfd2f2ccb350c587
ted816 <tohjiasen@gmail.com>
2025-04-29 16:45:49 +0800

    Create c_shell_scripting.c

---
+++ codes_and_notes/c_shell_scripting.c
@@ -0,0 +1,18 @@
+/*
+gcc -o new_tab c_shell_scripting.c
+./new_tab
+
+c execute faster than shell
+less flexibility than shell
+*/
+
+#include <stdlib.h>
+
+int main(void){
+
+  system("xfce4-terminal --version");
+  system("echo hello world")
+  system("xfce-terminal --tab");
+
+  return 0;
+}
