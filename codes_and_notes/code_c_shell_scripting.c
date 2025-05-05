3b0524a126bf75418073d2b6b30557b0d0e3e772
ted816 <tohjiasen@gmail.com>
2025-05-05 20:31:56 +0800

    Update code_c_shell_scripting.c

--- codes_and_notes/code_c_shell_scripting.c
+++ codes_and_notes/code_c_shell_scripting.c
@@ -3,7 +3,11 @@ gcc -o new_tab c_shell_scripting.c
 ./new_tab
 
 c execute faster than shell
-less flexibility than shell
+c is less flexibility than shell
+
+c execute as machine code hence faster
+compare to shell or python which is interpretive
+usage depends on requirements
 */
 
 #include <stdlib.h>
